92ec13fe4585c324d3b792e57ca328ab64341d9a
makcarosh <makarmironov08@gmail.com>
2026-04-15 09:15:08 +0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,29 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearNick
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+
+        public List<string> tags;
+        public void LoadDefaults()
+        {
+            tags = new List<string>
+            {
+                "roez",
+                "cobra",
+                "redline",
+                "recate",
+                "whyme",
+                "militaryserv",
+                "pandahut"
+            };
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using ClearNick;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Permissions;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NameCleanerPlugin : RocketPlugin<Configuration>
+{
+    protected override void Load()
+    {
+        UnturnedPermissions.OnJoinRequested += UnturnedPermissions_OnJoinRequested;
+        Rocket.Core.Logging.Logger.Log("Clear nick is load! \nDeveloper discord: makcarosh")
+    }
+
+    protected override void Unload()
+    {
+        UnturnedPermissions.OnJoinRequested -= UnturnedPermissions_OnJoinRequested;
+        Rocket.Core.Logging.Logger.Log("Clear nick is unload! \nDeveloper discord: makcarosh")
+    }
+    
+    private void UnturnedPermissions_OnJoinRequested(CSteamID player, ref ESteamRejection? rejectionReason)
+    {
+        SteamPending steamPending = Provider.pending.FirstOrDefault((SteamPending p) => p.playerID.steamID.m_SteamID == player.m_SteamID);
+
+        var config = Configuration.Instance;
+        List<string> tags = config.tags;   
+        string name = steamPending.playerID.characterName;
+        
+
+        foreach (string tag in tags)
+        {
+            int index = name.IndexOf(tag, StringComparison.OrdinalIgnoreCase);
+            if (index >= 0)
+            {
+                string cleaned = name.Remove(index, tag.Length).Trim();
+
+                steamPending.playerID.characterName = cleaned;
+            }
+        }
+    }
+}
